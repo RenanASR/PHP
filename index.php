c072cb2d180054630994b230d36909fbe6ab5d59
RenanASR <33239679+RenanASR@users.noreply.github.com>
2019-03-24 20:45:53 -0300

    Versão em inglês

--- index.php
+++ index.php
@@ -8,8 +8,10 @@
 		$hello = "Olá Mundo 123!";
 		$hcode = "Olá HCode!";
 		$ano = "2019";
+		$world = "Hello World 123!";
 	?>
 		<p><?= $hello; ?></p>
+		<p><?= $world; ?></p>
 		<hr />
 		<p>Estamos no ano de <?= "".$ano; ?></p>
 	</body>
